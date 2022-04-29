fe201b6cd3115d1e8bc5fd2a26b6a12a1c3b931d
Muhammad-Nafis-Abdullah <nafisabdullah2@gmail.com>
2022-04-29 15:12:29 +0600

    connect with git repo

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import NotFound from "./components/NotFound";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+
+
+  return (
+    <div className="">
+      
+      <h1 className="container border-4 text-center animate-pulse">React is Working....</h1>
+
+      <Routes>
+        <Route path="/" element={''}/>
+
+        
+
+        <Route path="/*" element={<NotFound/>}/>
+      </Routes>
+
+    </div>
+  );
+}
+
+export default App;

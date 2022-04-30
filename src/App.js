4cee7d49b714341eb9bf77d179df926684f35f4c
Muhammad-Nafis-Abdullah <nafisabdullah2@gmail.com>
2022-05-01 01:58:25 +0600

    dynamic title name and favicon added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 import NotFound from "./components/NotFound";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./components/Header";
 import Home from "./components/Home";
+import { useEffect } from "react";
 
 
 function App() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    const title = pathname.split('/');
+    document.title = `${title[1]} M.N.A Car Dealer`;
+  }, [pathname]);
 
 
   return (
